68c9842bc0148d261d08a5a7ccfd0ffe63dc57d6
benRenard <benjamin.renard@inrae.fr>
2025-03-04 17:04:36 +0100

    Augmented the class `mcmcOptions` to handle manual mode to specify the jump standard deviations. Addresses issue #9

--- R/class_mcmcOptions.R
+++ R/class_mcmcOptions.R
@@ -17,7 +17,11 @@
 #' @param upMult Numeric (>1, avoid 1/dowMult) multiplication factor used to increase
 #'     jump size when move rate is too high.
 #' @param multFactor Numeric >0, multiplicative factor to set initial jump standard deviations
-#'    to multFactor*|initValue|.
+#'    to multFactor*|initValue| (AUTO mode).
+#' @param manualMode logical, should jump standard deviations be entered manually?
+#' @param thetaStd Numeric vector (>0), jump standard deviations for model parameters theta (MANUAL mode).
+#' @param gammaStd list of numeric vectors (>0), size = number of output variables of the model.
+#'     Jump standard deviations for structural error parameters gamma of each output variable (MANUAL mode).
 #' @return An object of class 'mcmcOptions'.
 #' @examples
 #' m <- mcmcOptions()
@@ -26,8 +30,10 @@ mcmcOptions<-function(fname="Config_MCMC.txt",result.fname='Results_MCMC.txt',
                nAdapt=100,nCycles=100,
                minMoveRate=0.1,maxMoveRate=0.5,
                downMult=0.9,upMult=1.1,
-               multFactor=0.1){
-  o<-new_mcmcOptions(fname,result.fname,nAdapt,nCycles,minMoveRate,maxMoveRate,downMult,upMult,multFactor)
+               multFactor=0.1,manualMode=FALSE,
+               thetaStd=9999,gammaStd=list(9999)){
+  o<-new_mcmcOptions(fname,result.fname,nAdapt,nCycles,minMoveRate,maxMoveRate,downMult,upMult,multFactor,
+                     manualMode,thetaStd,gammaStd)
   return(validate_mcmcOptions(o))
 }
 
@@ -46,11 +52,10 @@ mcmcOptions<-function(fname="Config_MCMC.txt",result.fname='Results_MCMC.txt',
 toString.mcmcOptions<-function(x,...){
   value=list(x$result.fname,x$nAdapt,x$nCycles,
              x$minMoveRate,x$maxMoveRate,x$downMult,x$upMult,
-             0, # Mode for setting the initial Std of the jump distribution - 0=auto
+             ifelse(x$manualMode,1,0), # Mode for setting the initial Std of the jump distribution - 0=auto
              '****    DEFINITION OF INITIAL JUMP STD   ****', # just a cosmetics line
-             x$multFactor,
-             -9999,-9999 # manual mode stuff, disabled
-             )
+             x$multFactor, # multiplicative factor in auto mode
+             x$thetaStd)# jump stdevs for theta in manual mode
   comment=c(
     'result.fname, name of MCMC file (|!| name of the file only, not full path)',
     'nAdapt, adaptation period: jump sizes are increased/decreased every Nadapt iterations to comply with the desired moving rates',
@@ -61,10 +66,12 @@ toString.mcmcOptions<-function(x,...){
     'upMult, (>1, avoid 1/dowMult) multiplication factor used to increase jump size when move rate is too high',
     'Mode for setting the initial Std of the jump distribution - 0=auto[recommended]',
     'Cosmetic line, unused',
-    'multFactor, multiplicative factor to set initial jump standard deviations to multFactor*|initValue|',
-    'manual mode stuff, disabled',
-    'manual mode stuff, disabled'
-  )
+    'multFactor, multiplicative factor to set initial jump standard deviations to multFactor*|initValue| [AUTO mode]',
+    'Jump standard deviations for model parameters theta [MANUAL mode]')
+  for(i in 1:length(x$gammaStd)){
+    value=c(value,x$gammaStd[i])
+    comment=c(comment,paste('Jump standard deviations for structural error parameters gamma of output variable',i,'[MANUAL mode]'))
+  }
   txt<-toString_engine(value,comment)
   return(txt)
 }
@@ -84,8 +91,8 @@ is.mcmcOptions<-function(o){
 
 #***************************************************************************----
 # internal constructor ----
-new_mcmcOptions<-function(fname,result.fname,nAdapt,nCycles,
-                   minMoveRate,maxMoveRate,downMult,upMult,multFactor){
+new_mcmcOptions<-function(fname,result.fname,nAdapt,nCycles,minMoveRate,maxMoveRate,
+                          downMult,upMult,multFactor,manualMode,thetaStd,gammaStd){
   stopifnot(is.character(fname))
   stopifnot(is.character(result.fname))
   stopifnot(is.numeric(nAdapt))
@@ -95,9 +102,13 @@ new_mcmcOptions<-function(fname,result.fname,nAdapt,nCycles,
   stopifnot(is.numeric(downMult))
   stopifnot(is.numeric(upMult))
   stopifnot(is.numeric(multFactor))
+  stopifnot(is.logical(manualMode))
+  stopifnot(is.numeric(thetaStd))
+  stopifnot(is.list(gammaStd))
   o <- list(fname=fname,result.fname=result.fname,nAdapt=nAdapt,nCycles=nCycles,
             minMoveRate=minMoveRate,maxMoveRate=maxMoveRate,
-            downMult=downMult,upMult=upMult,multFactor=multFactor)
+            downMult=downMult,upMult=upMult,multFactor=multFactor,
+            manualMode=manualMode,thetaStd=thetaStd,gammaStd=gammaStd)
   class(o) <- 'mcmcOptions'
   return(o)
 }
@@ -122,6 +133,12 @@ validate_mcmcOptions<-function(x){
   if(x$upMult == 1/x$downMult){warning("avoid `upMult` = 1/`downMult` (oscillating adaptation)",call.=TRUE)}
   # multFactor: >0
   if(x$multFactor <= 0 ){stop("`multFactor` should be strictly positive",call.=FALSE)}
+  # thetaStd: >0
+  if( any(x$thetaStd <= 0) ){stop("`thetaStd` should be all be strictly positive",call.=FALSE)}
+  # gammaStd: >0
+  for(i in 1:length(gammaStd)){
+    if( any(x$gammaStd[[i]] <= 0) ){stop("`gammaStd` should be all be strictly positive",call.=FALSE)}
+  }
   return(x)
 }
 
