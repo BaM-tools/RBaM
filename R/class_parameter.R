8f82eeb0ca1bb05fbf06097139feddd1758a1f73
benRenard <benjamin.renard@inrae.fr>
2025-07-11 11:25:22 +0200

    New utility functions getInitPar and setInitPar

--- R/class_parameter.R
+++ R/class_parameter.R
@@ -43,6 +43,45 @@ toString.parameter<-function(x,...){
 }
 
 #***************************************************************************----
+# Utility functions ----
+
+#' Get initial values
+#'
+#' Get the initial values of a list of parameters and return them as a vector.
+#'
+#' @param parameters List of parameter objects, parameters whose initial values are sought.
+#' @return A numeric vector containing the initial values
+#' @examples
+#' ps <- list(parameter(name='par1',init=0),parameter(name='par2',init=1))
+#' getInitPar(ps)
+#' @export
+getInitPar <- function(parameters){
+  out=sapply(parameters,function(x){x$init})
+  return(out)
+}
+
+#' Set initial values
+#'
+#' Set the initial values of a list of parameters.
+#'
+#' @param parameters List of parameter objects, parameters whose initial values are to be set.
+#' @param values Numeric vector, initial values.
+#' @return a list of parameter objects equal to the input list 'parameters', except
+#'     for their initial values.
+#' @examples
+#' ps <- list(parameter(name='par1',init=0),parameter(name='par2',init=1))
+#' ps=setInitPar(ps,c(10,100))
+#' print(ps)
+#' @export
+setInitPar <- function(parameters,values){
+  if(length(parameters)!=length(values)){
+    stop("`parameters` and `values` should have the same length",call.=FALSE)
+  }
+  for(i in 1:length(parameters)){parameters[[i]]$init=values[i]}
+  return(parameters)
+}
+
+#***************************************************************************----
 # is function ----
 #' parameter tester
 #'
