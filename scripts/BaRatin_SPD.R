a348d96dcaae19c7de2c7a7467f1d19c871bb957
benRenard <benjamin.renard@inrae.fr>
2025-07-17 17:22:28 +0200

    Continued implementing SPD script

--- scripts/BaRatin_SPD.R
+++ scripts/BaRatin_SPD.R
@@ -1,8 +1,7 @@
 library(RBaM)
 
 # BaRatin flavor
-BaRatinFlavor='BaRatin' # 'BaRatinBAC'
-remnant=rep(list(remnantErrorModel()),1)
+BaRatinFlavor='BaRatinBAC' # should work also with 'BaRatin' - what's the safest default?
 
 # Calibration data
 H=MeyrasGaugings$h
@@ -14,34 +13,34 @@ controlMatrix=rbind(c(1,0,0),c(0,1,0),c(0,1,1))
 
 # Declare variable parameters.
 # Only b/k's and a's are allowed to be VAR (all c's are stable).
-kVAR=c(TRUE, TRUE, FALSE) # k's for first 2 controls (k1 and k2) are VAR
+bVAR=c(TRUE, TRUE, FALSE) # b's for first 2 controls (k1 and k2) are VAR
 aVAR=c(TRUE, FALSE, FALSE) # a for first control (a1) is VAR
 
 # Define priors as usual.
 # For VAR parameters, they will be interpreted as the prior for period 1.
-# Also for VAR parameters, the prior distribution for k/b's has to be 'Gaussian',
+# Also for VAR parameters, the prior distribution for b/k's has to be 'Gaussian',
 # while the prior distribution for a's has to be 'LogNormal'.
-k1=parameter(name='k1',init=-0.6,prior.dist='Gaussian',prior.par=c(-0.6,0.5))
+b1=parameter(name='b1',init=-0.6,prior.dist='Gaussian',prior.par=c(-0.6,0.5))
 a1=parameter(name='a1',init=exp(2.65),prior.dist='LogNormal',prior.par=c(2.65,0.35))
 c1=parameter(name='c1',init=1.5,prior.dist='Gaussian',prior.par=c(1.5,0.025))
-k2=parameter(name='k2',init=0,prior.dist='Gaussian',prior.par=c(0,0.5))
+b2=parameter(name='b2',init=0,prior.dist='Gaussian',prior.par=c(-0.6,0.5))
 a2=parameter(name='a2',init=exp(3.28),prior.dist='LogNormal',prior.par=c(3.28,0.33))
 c2=parameter(name='c2',init=1.67,prior.dist='Gaussian',prior.par=c(1.67,0.025))
-k3=parameter(name='k3',init=1.2,prior.dist='Gaussian',prior.par=c(1.2,0.2))
+b3=parameter(name='b3',init=1.2,prior.dist='Gaussian',prior.par=c(1.2,0.2))
 a3=parameter(name='a3',init=exp(3.48),prior.dist='LogNormal',prior.par=c(3.46,0.38))
 c3=parameter(name='c3',init=1.67,prior.dist='Gaussian',prior.par=c(1.67,0.025))
-allPars=list(k1,a1,c1,k2,a2,c2,k3,a3,c3)
+pars=list(b1,a1,c1,b2,a2,c2,b3,a3,c3)
 
 # Define properties of VAR parameters.
 # 1. Prior parameters for incremental changes.
-# For k/b's, the 2 parameters are the mean/sd of the Gaussian prior for ADDITIVE incremental changes.
+# For b/k's, the 2 parameters are the mean/sd of the Gaussian prior for ADDITIVE incremental changes.
 # For a's, the 2 parameters are the meanlog/sdlog of the LogNormal prior for MULTIPLICATIVE incremental changes.
 # Parameters should be put in a named list, with the names corresponding to the names of the parameters that have been declared variable.
-deltaPars=list(k1=c(0,0.5),a1=c(0,0.2),k2=c(0,0.5))
+deltaPars=list(b1=c(0,0.25),a1=c(0,0.2),b2=c(0,0.5))
 # 2. Periods
 # Periods for each VAR parameter should be put in a named list as previously.
 # Periods do not need to be the same for all VAR parameters - see a1 below for example.
-periods=list(k1=MeyrasGaugings$Period,a1=c(rep(1,49),rep(2,55)),k2=MeyrasGaugings$Period)
+periods=list(b1=MeyrasGaugings$Period,a1=c(rep(1,49),rep(2,55)),b2=MeyrasGaugings$Period)
 # 3. Number of periods
 # Number of periods for each VAR parameter should be put in a named list as previously.
 # In general this will just be the max of each period defined previously, but not always:
@@ -63,6 +62,7 @@ nPeriods=lapply(periods,max)
 #' @examples
 #' res=getVARproperties(p1Par=c(2,0.1),deltaPar=c(0,0.3),nPeriod=25)
 #' image(res$cor)
+#' @export
 #' @importFrom stats cov2cor
 getVARproperties <- function(p1Par,deltaPar,nPeriod){
   if(nPeriod<=1){stop('nPeriod should be strictly larger than 1',call.=FALSE)}
@@ -86,6 +86,7 @@ getVARproperties <- function(p1Par,deltaPar,nPeriod){
 #' @return A square matrix.
 #' @examples
 #' blocDiag(list(1,cbind(c(1,2),c(3,4)),25))
+#' @export
 blocDiag <- function(blocs){
   n=sum(sapply(blocs,NROW))
   M=diag(1,n,n)
@@ -98,77 +99,104 @@ blocDiag <- function(blocs){
   return(M)
 }
 
-workspace=file.path(getwd(),'BaM_workspace')
-nControl=NROW(controlMatrix)
-parlist=corlist=vector('list',length(allPars))
-names(parlist)=getNames(allPars)
 
-dfperiods=as.data.frame(periods)
-names(dfperiods) <- paste0(names(dfperiods),'_period')
-D=dataset(X=data.frame(H=H),Y=data.frame(Q=Q),Yu=data.frame(uQ=uQ),VAR.indx=dfperiods,data.dir=workspace)
-m=0
-for(i in 1:nControl){
-  # k
-  m=m+1
-  if(kVAR[i]){
-    nam=allPars[[m]]$name
-    if(!(nam %in% names(deltaPars))){
-      mess=paste('Parameter',nam,'not found in named list deltaPars.')
-      stop(mess,call.=FALSE)
+runSPD <-function(workspace,controlMatrix,pars,
+                  bVAR,aVAR,deltaPars,periods,
+                  H,Q,uQ=0*Q,nPeriods=lapply(periods,max),
+                  BaRatinFlavor='BaRatinBAC',
+                  remnant=remnantErrorModel(funk='Linear',
+                                            par=list(parameter('g1',1,'LogNormal',c(0,10)),
+                                                     parameter('g2',0.1,'LogNormal',c(log(0.1),10))))
+                  ){
+  hmax=max(H)+2*diff(range(H))
+  nControl=NROW(controlMatrix)
+  parlist=corlist=vector('list',length(pars))
+  names(parlist)=getNames(pars)
+
+  dfperiods=as.data.frame(periods)
+  names(dfperiods) <- paste0(names(dfperiods),'_period')
+  D=dataset(X=data.frame(H=H),Y=data.frame(Q=Q),Yu=data.frame(uQ=uQ),VAR.indx=dfperiods,data.dir=workspace)
+  m=0
+  for(i in 1:nControl){
+    # k
+    m=m+1
+    if(bVAR[i]){
+      nam=pars[[m]]$name
+      if(!(nam %in% names(deltaPars))){
+        mess=paste('Parameter',nam,'not found in named list deltaPars.')
+        stop(mess,call.=FALSE)
+      }
+      if(!(nam %in% names(nPeriods))){
+        mess=paste('Parameter',nam,'not found in named list nPeriods')
+        stop(mess,call.=FALSE)
+      }
+      np=nPeriods[[nam]]
+      props=getVARproperties(p1Par=pars[[m]]$prior$par,deltaPar=deltaPars[[nam]],nPeriod=np)
+      priorpars=split(cbind(props$mean,props$sd),seq(np))
+      foo=parameter_VAR(name=nam, # parameter name
+                        index=paste0(nam,'_period'), # Name of the column in D$VAR.indx containing the period index
+                        d=D, # The dataset mentioned above
+                        init=props$mean, # first guesses
+                        prior.dist=rep('Gaussian',np), # prior distributions
+                        prior.par=priorpars # prior parameters
+      )
+      parlist[[m]]=foo
+      corlist[[m]]=props$cor
+    } else {
+      parlist[[m]]=pars[[m]]
+      corlist[[m]]=1
     }
-    if(!(nam %in% names(nPeriods))){
-      mess=paste('Parameter',nam,'not found in named list nPeriods')
-      stop(mess,call.=FALSE)
+    # a
+    m=m+1
+    if(aVAR[i]){
+      nam=pars[[m]]$name
+      if(!(nam %in% names(deltaPars))){
+        mess=paste('Parameter',nam,'not found in named list deltaPars.')
+        stop(mess,call.=FALSE)
+      }
+      if(!(nam %in% names(nPeriods))){
+        mess=paste('Parameter',nam,'not found in named list nPeriods')
+        stop(mess,call.=FALSE)
+      }
+      np=nPeriods[[nam]]
+      props=getVARproperties(p1Par=pars[[m]]$prior$par,deltaPar=deltaPars[[nam]],nPeriod=np)
+      priorpars=split(cbind(props$mean,props$sd),seq(np))
+      foo=parameter_VAR(name=nam, # parameter name
+                        index=paste0(nam,'_period'), # Name of the column in D$VAR.indx containing the period index
+                        d=D, # The dataset mentioned above
+                        init=exp(props$mean), # first guesses
+                        prior.dist=rep('LogNormal',np), # prior distributions
+                        prior.par=priorpars # prior parameters
+      )
+      parlist[[m]]=foo
+      corlist[[m]]=props$cor
+    } else {
+      parlist[[m]]=pars[[m]]
+      corlist[[m]]=1
     }
-    np=nPeriods[[nam]]
-    props=getVARproperties(p1Par=allPars[[m]]$prior$par,deltaPar=deltaPars[[nam]],nPeriod=np)
-    priorpars=split(cbind(props$mean,props$sd),seq(np))
-    foo=parameter_VAR(name=nam, # parameter name
-                      index=paste0(nam,'_period'), # Name of the column in D$VAR.indx containing the period index
-                      d=D, # The dataset mentioned above
-                      init=props$mean, # first guesses
-                      prior.dist=rep('Gaussian',np), # prior distributions
-                      prior.par=priorpars # prior parameters
-    )
-    parlist[[m]]=foo
-    corlist[[m]]=props$cor
-  } else {
-    parlist[[m]]=allPars[[m]]
+    # c
+    m=m+1
+    parlist[[m]]=pars[[m]]
     corlist[[m]]=1
   }
-  # a
-  m=m+1
-  if(aVAR[i]){
-    nam=allPars[[m]]$name
-    if(!(nam %in% names(deltaPars))){
-      mess=paste('Parameter',nam,'not found in named list deltaPars.')
-      stop(mess,call.=FALSE)
-    }
-    if(!(nam %in% names(nPeriods))){
-      mess=paste('Parameter',nam,'not found in named list nPeriods')
-      stop(mess,call.=FALSE)
-    }
-    np=nPeriods[[nam]]
-    props=getVARproperties(p1Par=allPars[[m]]$prior$par,deltaPar=deltaPars[[nam]],nPeriod=np)
-    priorpars=split(cbind(props$mean,props$sd),seq(np))
-    foo=parameter_VAR(name=nam, # parameter name
-                      index=paste0(nam,'_period'), # Name of the column in D$VAR.indx containing the period index
-                      d=D, # The dataset mentioned above
-                      init=exp(props$mean), # first guesses
-                      prior.dist=rep('LogNormal',np), # prior distributions
-                      prior.par=priorpars # prior parameters
-    )
-    parlist[[m]]=foo
-    corlist[[m]]=props$cor
+  corlist=c(corlist,rep(1,length(remnant$par)))
+  cormat=blocDiag(corlist)
+
+  if(BaRatinFlavor=='BaRatinBAC'){
+    foo=list(controlMatrix,hmax)
   } else {
-    parlist[[m]]=allPars[[m]]
-    corlist[[m]]=1
+    foo=controlMatrix
   }
-  # c
-  m=m+1
-  parlist[[m]]=allPars[[m]]
-  corlist[[m]]=1
+  M=model(ID=BaRatinFlavor,nX=1,nY=1,par=parlist,
+          xtra=xtraModelInfo(object=foo))
+  write.table(cormat,file=file.path(workspace,'PriorCorrelation.txt'),
+              row.names=FALSE,col.names=FALSE)
+  BaM(workspace=workspace,mod=M,data=D,remnant=list(remnant),preClean=FALSE)
 }
-corlist=c(corlist,rep(1,length(remnant[[1]]$par)))
-cormat=blocDiag(corlist)
 
+workspace=file.path(getwd(),'BaM_workspace')
+runSPD(workspace=workspace,controlMatrix=controlMatrix,pars=pars,
+       bVAR=bVAR,aVAR=aVAR,deltaPars=deltaPars,periods=periods,
+       H=H,Q=Q,uQ=uQ)
+mcmc=readMCMC(file.path(workspace,'Results_Cooking.txt'))
+violinPlot(mcmc[,c(1:5,9:13)])
