a95adc5d018885054aabe3bb7106fc5035e99722
benRenard <benjamin.renard@inrae.fr>
2025-07-18 14:32:56 +0200

    Final minor improvements to SPD script

--- scripts/BaRatin_SPD.R
+++ scripts/BaRatin_SPD.R
@@ -1,21 +1,24 @@
 library(RBaM)
 
-# BaRatin flavor
-BaRatinFlavor='BaRatinBAC' # should work also with 'BaRatin' - what's the safest default?
+# Define BaRatin flavor.
+# It should also work with 'BaRatin', need to think about what is the best default.
+BaRatinFlavor='BaRatinBAC'
 
 # Calibration data
 H=MeyrasGaugings$h
 Q=MeyrasGaugings$Q
 uQ=MeyrasGaugings$uQ
 
-# Define control matrix: columns are controls, rows are stage ranges.
+# Define control matrix.
 controlMatrix=rbind(c(1,0,0),c(0,1,0),c(0,1,1))
 
 # Declare variable parameters.
-bVAR=c(TRUE, TRUE, FALSE) # b's for first 2 controls (k1 and k2) are VAR
+# Note that exponents c cannot be declared variable.
+bVAR=c(TRUE, TRUE, FALSE) # b's for first 2 controls (b1 and b2) are VAR
 aVAR=c(TRUE, FALSE, FALSE) # a for first control (a1) is VAR
 
-# Define priors.
+# Define priors. Note that for parameters declared as variable (here b1, a1 and b2),
+# the specified prior applies to period 1.
 b1=parameter(name='b1',init=-0.6,prior.dist='Gaussian',prior.par=c(-0.6,0.5))
 a1=parameter(name='a1',init=exp(2.65),prior.dist='LogNormal',prior.par=c(2.65,0.35))
 c1=parameter(name='c1',init=1.5,prior.dist='Gaussian',prior.par=c(1.5,0.025))
@@ -29,9 +32,10 @@ pars=list(b1,a1,c1,b2,a2,c2,b3,a3,c3)
 
 # Define properties of VAR parameters.
 # 1. Prior parameters for incremental changes.
-# For b/k's, the 2 parameters are the mean/sd of the Gaussian prior for ADDITIVE incremental changes.
-# For a's, the 2 parameters are the meanlog/sdlog of the LogNormal prior for MULTIPLICATIVE incremental changes.
-# Parameters should be put in a named list, with the names corresponding to the names of the parameters that have been declared variable.
+# Should be put in a named list, with the names corresponding to the names of the parameters that have been declared variable.
+# For b/k's, the 2 values are the mean/sd of the Gaussian prior for ADDITIVE incremental changes.
+# For a's, the 2 values are the meanlog/sdlog of the LogNormal prior for MULTIPLICATIVE incremental changes.
+# Might need to implement some translator in BaRatinAGE to express these as values +/- extended U.
 deltaPars=list(b1=c(0,0.25),a1=c(0,0.2),b2=c(0,0.5))
 # 2. Periods
 # Periods for each VAR parameter should be put in a named list as previously.
@@ -43,9 +47,12 @@ periods=list(b1=MeyrasGaugings$Period,a1=c(rep(1,49),rep(2,55)),b2=MeyrasGauging
 # there could be one or several additional periods with no gaugings.
 nPeriods=lapply(periods,max)
 
-
+# Ready to estimate
 workspace=file.path(getwd(),'BaM_workspace')
 mcmc=SPD_estimate(workspace=workspace,controlMatrix=controlMatrix,pars=pars,
-                  bVAR=bVAR,aVAR=aVAR,deltaPars=deltaPars,periods=periods,
+                  bVAR=bVAR,aVAR=aVAR,deltaPars=deltaPars,periods=periods,nPeriods=nPeriods,
                   H=H,Q=Q,uQ=uQ)
-violinPlot(mcmc[,c(1:5,9:13)])
+# Quick look at results
+violinPlot(mcmc[,c(1:5)])+ggplot2::labs(title='b1 across 5 periods')
+violinPlot(mcmc[,c(6:7)])+ggplot2::labs(title='a1 across 2 periods')
+violinPlot(mcmc[,c(9:13)])+ggplot2::labs(title='b2 across 5 periods')
